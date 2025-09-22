6272b0bb619047e220cb094c62b4c34d4083b979
Alden G <aldengrover@gmail.com>
2025-09-22 17:19:46 -0400

    Updated state machine for encoder

--- Firmware/include/encoder.h
+++ Firmware/include/encoder.h
@@ -3,12 +3,14 @@
 
 namespace enc {
     enum quad_state : uint8_t {
-        PREV_LEFT = 0,
-        PREV_RIGHT = 1
+        PREV_LEFT_HIGH = 0,
+        PREV_LEFT_LOW = 1,
+        PREV_RIGHT_HIGH = 2,
+        PREV_RIGHT_LOW = 3
     };
     
-    void init(uint32_t p0, uint32_t p1, bool ccw_pos); // attach pin interrupts to p0, p1 that inc & dec based on state machine
+    void init(uint32_t p0, uint32_t p1, uint64_t steps_per_rev, bool ccw_pos); // attach pin interrupts to p0, p1 that inc & dec based on state machine
     void reset(); // sets current position to 0
-    void get_pos(); // reads value that pin interrupt has calculated
+    int64_t get_pos(); // reads value that pin interrupt has calculated
 
 }
