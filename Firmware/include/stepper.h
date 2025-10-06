104f7a0e742514285a21c28cb4585433ade24744
Alden G <aldengrover@gmail.com>
2025-10-06 11:06:39 -0400

    updated stepper header file

--- Firmware/include/stepper.h
+++ Firmware/include/stepper.h
@@ -1,15 +1,22 @@
 #pragma once
 #include <cstdint>
+#include <Arduino.h>
+
+#define DIR_WAIT 10
+#define PUL_WAIT 10
+#define CW_DIR HIGH
+#define CCW_DIR LOW
 
 namespace step_dir { // may change to interrupt driven system later
     void init(uint64_t step_per_rev, uint32_t step_pin, uint32_t dir_pin);
     void pulseCCW();
     void pulseCW();
-    void moveCW(uint64_t num); // call pulseCW in a loop
-    void moveCCW(uint64_t num); // call pulseCCW in a loop
+    void moveCW(uint64_t num); // call pulseCW in a loop, does not reutrn until complete
+    void moveCCW(uint64_t num); // call pulseCCW in a loop, does not return until complete
+    void move(int64_t num, bool ccw_pos); // moves correct number of CW/CCW steps, num calculated in transform
 }
 
 namespace positioning {
     void init(uint64_t steps_per_rev, uint64_t rev_per_mm, bool ccw_pos);
-    uint64_t transform(uint64_t m, bool ccw); // millimeters, if right_pos then ccw is true when m>0
+    int64_t transform(int64_t m); // m in millimeters, if right_pos then ccw is true when m>0
 }
